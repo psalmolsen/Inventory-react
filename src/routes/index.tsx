0158219584e96eeafc65c49795792281b832c09c
psalmolsen <psalmolsennaval@gmail.com>
2026-07-02 15:56:18 +0800

    transition to react

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useMemo, useState, useEffect } from "react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   getTabsFn,
   getMaterialsFn,
